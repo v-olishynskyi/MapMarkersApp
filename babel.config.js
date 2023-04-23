2ede5cd78d2fe9d13f6a243f7fc4ab32649737dc
Vladyslav Olishynskyi <vlad.olishynskyi.work@gmail.com>
2023-04-23 23:46:57 +0300

    add typescript

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,43 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    '@babel/plugin-proposal-export-namespace-from',
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.android.js',
+          '.android.jsx',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.jsx',
+          '.ios.tsx',
+          '.web.js',
+          '.web.jsx',
+          '.web.tsx',
+          '.native.js',
+          '.native.tsx',
+        ],
+        root: ['.'],
+        alias: {
+          '@navigation': './src/navigation',
+          '@components': './src/components',
+          '@assets': './src/assets',
+          '@api': './src/api',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+          '@store': './src/store',
+          '@styles': './src/styles',
+          '@env': './src/environments',
+          '@cometchat': './src/cometchat',
+          '@layout': './src/layout',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
