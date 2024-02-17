75316c30f6b673b604f10ee6e299dde70016e3ec
Vladyslav Olishynskyi <vlad.olishynskyi.work@gmail.com>
2024-02-17 21:50:43 +0200

    refactor: move menu component to layout components

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -4,7 +4,6 @@ export { default as Avatar } from './Avatar/Avatar';
 export { default as BaseList } from './BaseList/BaseList';
 export { default as LoaderRefresh } from './LoaderRefresh/LoaderRefresh';
 export { default as Pressable } from './Pressable/Pressable';
-export { default as Menu } from './Menu/Menu';
 export { default as MenuItem } from './MenuItem/MenuItem';
 export { default as Loader } from './Loader/Loader';
 export { default as IconButton } from './IconButton/IconButton';
